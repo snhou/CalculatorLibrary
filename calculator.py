a0879c49e734b65d147b61dc9023a57bb1fc9240
Shawn Hou <shawnhou@ShawndeMacBook-Pro.local>
2024-01-05 16:02:50 +0800

    modify_format

--- calculator.py
+++ calculator.py
@@ -2,11 +2,14 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
 
 def multiply(first_term, second_term):
     return first_term * second_term * 2
